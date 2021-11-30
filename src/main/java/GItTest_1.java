8dc683eaf2cda15109a73cde7022ac41e8e325ce
xiaohui <2216159207@qq.com>
2021-11-30 14:58:34 +0800

    hot_fix commit

--- src/main/java/GItTest_1.java
+++ src/main/java/GItTest_1.java
@@ -2,5 +2,6 @@ public class GItTest_1 {
     public static void main(String[] args) {
         System.out.println("hello1");
         System.out.println("hello2");
+        System.out.println("hot_fix hello3");
     }
 }
